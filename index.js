67529bd7f595072307230c2455bdc79c1c658df8
tarasabella <tarasabella@gmail.com>
2019-02-05 15:48:27 -0500

    finished

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const myUser = {"id":1, "username":"pouros"}
+
   const list = document.getElementById("list")
   const showPanel = document.getElementById("show-panel")
   displayBooks();
@@ -21,18 +23,20 @@ document.addEventListener("DOMContentLoaded", function() {
     if (event.target.classList.contains("book-on-sidebar")) {
       fetch(`http://localhost:3000/books/${event.target.dataset.id}`)
       .then(res => res.json())
-      .then(book => {
-        string = `<h1 class="book-title"> ${book.title} </h1>
-        <img src = "${book.img_url}" class="book-img-url">
-        <p class="book-description"> ${book.description} </p>
-        <button type = "button" class="like-button"> LIKE </button>`
-
-        showPanel.innerHTML = string;
-        showPanel.append(makeUsersList(book));
-        showPanel.dataset.id = event.target.dataset.id;
-      })
+      .then(book => showBookOnPanel(book))
     }
   })
+
+  const showBookOnPanel = (book) => {
+    string = `<h1 class="book-title"> ${book.title} </h1>
+    <img src = "${book.img_url}" class="book-img-url">
+    <p class="book-description"> ${book.description} </p>
+    <button type = "button" class="like-button"> LIKE </button>`
+
+    showPanel.innerHTML = string;
+    showPanel.append(makeUsersList(book));
+    showPanel.dataset.id = book.id;
+  }
 
   const makeUsersList = book => {
     let usersList = document.createElement("ul")
@@ -46,28 +50,41 @@ document.addEventListener("DOMContentLoaded", function() {
 
   showPanel.addEventListener("click", event => {
     if (event.target.classList.contains("like-button")) {
+      fetch(`http://localhost:3000/books/${event.target.parentNode.dataset.id}`)
+      .then(res => res.json())
+      .then(data => { updateBook(data)})
+
+
       // console.log("like-button")
+      //
+      // const usersList = document.querySelector(".users-list")
+      // let newUsersArray = []
+      // usersList.childNodes.forEach(li => {
+      //   const userObject = {id: li.dataset.id, username: li.innerText}
+      //   newUsersArray.push(userObject)
+      // })
 
-      const usersList = document.querySelector(".users-list")
-
-
-
-    fetch(`http://localhost:3000/books/${event.target.parentNode.dataset.id}`, {
-      method: "PATCH",
-      body: JSON.stringify({
-        id: event.target.parentNode.dataset.id,
-        title: document.querySelector(".book-title").innerText,
-        img_url: document.querySelector(".book-img-url").src,
-        description: document.querySelector(".book-description").innerText
-        users: []
-        // title: event.target.parentNode.querySelector()
-      }),
-      headers: {
-        "Content-Type": "application/json"
+      const updateBook = book => {
+        book.users.push(myUser)
+        fetch(`http://localhost:3000/books/${book.id}`, {
+          method: "PATCH",
+          body: JSON.stringify({
+            id: book.id,
+            title: book.title,
+            img_url: book.img_url,
+            description: book.description,
+            users: book.users
+            // title: event.target.parentNode.querySelector()
+          }),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        .then(res => res.json())
+        .then(book => showBookOnPanel(book))
       }
-    })
-    .then(res => res.json())
-    .then(data => console.log(data))
+
+
   }
 })
 
